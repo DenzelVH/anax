63f92237c3e7a7c46b7cec7255a387247fe32ea4
Denzel Wendy <denzel.wendy@gmail.com>
2025-07-26 12:34:44 +0700

    remove unused services

--- hosts/t440p/default.nix
+++ hosts/t440p/default.nix
@@ -12,9 +12,7 @@
     ./services/gnome-keyring.nix
     ./services/greetd.nix
     ./services/gvfs.nix
-    ./services/mariadb.nix
     ./services/pipewire.nix
-    ./services/postgresql.nix
     ./virtualisation/containers.nix
     ./virtualisation/docker.nix
     ./virtualisation/podman.nix
