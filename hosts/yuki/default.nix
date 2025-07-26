13fc764ed7dc260dbe29e1dabe9b061dcf098d41
Denzel Wendy <denzel.wendy@gmail.com>
2025-07-26 09:25:21 +0700

    remove nvidia configurations

--- hosts/yuki/default.nix
+++ hosts/yuki/default.nix
@@ -24,7 +24,6 @@
     ../config/hardware/bluetooth
     ../config/hardware/cpu/intel
     ../config/hardware/gpu/intel
-    ../config/hardware/gpu/nvidia
     ../config/hardware/ssd
     ../config/window-managers/hyprland
   ];
@@ -36,15 +35,6 @@
       enable = true;
       enable32Bit = true;
     };
-
-    nvidia = {
-      prime = {
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-
-    nvidia-container-toolkit.enable = true;
   };
 
   services = {
@@ -55,9 +45,7 @@
     hardware.bolt.enable = true;
   };
 
+  # TODO: Change to appropriate
   # Intel Core i7-12700H (Alder Lake)
   boot.kernelParams = ["i915.force_probe=46a6" "i915.enable_guc=3"];
-
-  # NVIDIA GeForce RTX 3050 Mobile (Ampere)
-  services.xserver.videoDrivers = ["nvidia"];
 }
